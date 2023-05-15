007f6c6a0b929c0ccaabb0492f38a31378e3f97e
liamreeves <liamreeves42@gmail.com>
2023-05-15 09:56:07 +0100

    Added styling for user submit

--- src/components/AddTrick.js
+++ src/components/AddTrick.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "../styles/Home.module.css"
+import styles from "../styles/Home.module.css";
 
 export default function AddTrick(props) {
   const [newTrick, setNewTrick] = useState("");
@@ -9,44 +9,46 @@ export default function AddTrick(props) {
     <>
       <div>
         <input
+          className="rounded text-black p-1"
           value={newTrick}
           onChange={(e) => setNewTrick(e.target.value)}
           type="text"
           maxLength={30}
         />
         <div>
-        <select
-          value={rotation}
-          onChange={(e) => setRotation(e.target.value)}
-          name="rotation"
-          id="rotation"
-        >
-          <option value="180">180</option>
-          <option value="360">360</option>
-          <option value="540">540</option>
-          <option value="720">720</option>
-          <option value="900">900</option>
-          <option value="1080">1080</option>
-        </select>
-        <select
-          value={stance}
-          onChange={(e) => setStance(e.target.value)}
-          name="stance"
-          id="stance"
-        >
-          <option value=""></option>
-          <option value="Switch">Switch</option>
-          <option value="Fakie">Fakie</option>
-          <option value="Nollie">Nollie</option>
-        </select>
-
+          <select
+            className="text-black p-1 m-1 rounded"
+            value={rotation}
+            onChange={(e) => setRotation(e.target.value)}
+            name="rotation"
+            id="rotation"
+          >
+            <option value="180">180</option>
+            <option value="360">360</option>
+            <option value="540">540</option>
+            <option value="720">720</option>
+            <option value="900">900</option>
+            <option value="1080">1080</option>
+          </select>
+          <select
+            className="text-black p-1 m-1 rounded"
+            value={stance}
+            onChange={(e) => setStance(e.target.value)}
+            name="stance"
+            id="stance"
+          >
+            <option value=""></option>
+            <option value="Switch">Switch</option>
+            <option value="Fakie">Fakie</option>
+            <option value="Nollie">Nollie</option>
+          </select>
         </div>
         <button
-        className={styles.button}
+          className={styles.button}
           onClick={() => {
-            props.setTrick(newTrick)
-              props.setRotation(rotation)
-              props.setStance(stance)
+            props.setTrick(newTrick);
+            props.setRotation(rotation);
+            props.setStance(stance);
           }}
         >
           Submit
