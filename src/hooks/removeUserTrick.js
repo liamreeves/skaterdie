9a90498d52837a7edc3f77dc3f28e77aa8eef07b
liamreeves <liamreeves42@gmail.com>
2023-05-18 17:32:25 +0100

    Added react-router & react-select and the changed layout.

--- src/hooks/removeUserTrick.js
+++ src/hooks/removeUserTrick.js
@@ -7,7 +7,6 @@ export default function removeUserTrick(user, stance, rotation, trickName) {
 
   // Get a reference to the user's trick list in the database
   const userTrickListRef = ref(db, "users/" + user + "/tricks/" + trickName + (stance ? "/" + stance : ""));
-  console.log(userTrickListRef)
 
   // Use the Firebase Realtime Database SDK to add the new trick to the list
   remove(userTrickListRef);
--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import AuthContextProvider from "./contexts/AuthContext";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
